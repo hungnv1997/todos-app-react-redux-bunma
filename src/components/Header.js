e6a7dd2717fa641871e0762a9f5c6a650c8686a2
Nguyen Viet Hung <hung.nv155790.hust@gmail.com>
2021-08-31 22:22:45 +0700

    Change max length Input

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ function Header(props) {
         <div  className='block header'>
             <h1 className="is-size-1 has-text-centered block">Todos App</h1>
             <div className=" block">
-            <input className="input block header__input" placeholder="...Add your todo" type="text" id="add-input" onChange={handleOnChangeInput} maxLength="15"/>
+            <input className="input block header__input" placeholder="...Add your todo" type="text" id="add-input" onChange={handleOnChangeInput} maxLength="20"/>
             <button className="button is-primary  is-fullwidth" onClick={handleClickAddBtn}>Add</button>
             </div>
         </div>
